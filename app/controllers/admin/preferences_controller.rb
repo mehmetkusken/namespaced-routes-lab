02dc237f46a43cfe707ee1af653344f8630cf58f
Mehmet <mehmetkusken@gmail.com>
2021-07-08 14:48:23 -0600

    commit tree fff550593b7ddb3b172de203ec515b202feb5a8b

---
+++ app/controllers/admin/preferences_controller.rb
@@ -0,0 +1,18 @@
+class Admin::PreferencesController < ApplicationController
+
+	def index
+	end
+
+	def song_sort_order
+  	end
+
+  	def artist_sort_order
+  	end
+
+  	def create_new_song
+  	end
+
+  	def create_new_artist
+  	end
+
+end 
--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,11 @@
 class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
+    if @preferences && @preferences.artist_sort_order
+      @artists = Artist.order(name: @preferences.artist_sort_order)
+    else
+      @artists = Artist.all
+    end
   end
 
   def show
@@ -9,44 +14,44 @@ class ArtistsController < ApplicationController
 
   def new
     @artist = Artist.new
+    if Preference.all[0].allow_create_artists == true
+      @artist = Artist.new
+    else
+      redirect_to artists_path
+    end
   end
 
   def create
     @artist = Artist.new(artist_params)
-
     if @artist.save
       redirect_to @artist
     else
       render :new
     end
   end
-
   def edit
     @artist = Artist.find(params[:id])
   end
-
   def update
     @artist = Artist.find(params[:id])
-
     @artist.update(artist_params)
-
     if @artist.save
       redirect_to @artist
     else
       render :edit
     end
   end
-
   def destroy
     @artist = Artist.find(params[:id])
     @artist.destroy
     flash[:notice] = "Artist deleted."
     redirect_to artists_path
   end
-
   private
-
   def artist_params
     params.require(:artist).permit(:name)
+  end
+  def set_preferences
+    @preferences = Admin::Preference.last
   end
 end
